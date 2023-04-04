8b4e743a9dfd2edbad380e8769c584a7fe2807da
asleepyfish <asleepyfish@foxmail.com>
2023-04-04 17:43:32 +0800

    support stream

--- src/main/java/com/asleepyfish/controller/ChatGPTController.java
+++ src/main/java/com/asleepyfish/controller/ChatGPTController.java
@@ -1,11 +1,16 @@
 package com.asleepyfish.controller;
 
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 import io.github.asleepyfish.util.OpenAiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,9 +26,22 @@ public class ChatGPTController {
     }
 
 
-
     @GetMapping("/downloadImage")
     public void downloadImage(String prompt, HttpServletResponse response) {
         OpenAiUtils.downloadImage(prompt, response);
+    }
+
+    @PostMapping("/streamChatTest")
+    public void streamChatTest(String content) {
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(Collections.singletonList(new ChatMessage(ChatMessageRole.USER.value(), content)))
+                .n(1)
+                .logitBias(new HashMap<>())
+                .stream(true)
+                .user("user")
+                .build();
+        OpenAiUtils.createStreamChatCompletion(chatCompletionRequest);
     }
 }
