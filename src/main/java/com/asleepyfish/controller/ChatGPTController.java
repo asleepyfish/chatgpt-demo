874f2f98aaa5b50fbdbd1e42ce2370a68e2c5702
asleepyfish <asleepyfish@foxmail.com>
2023-04-05 21:51:34 +0800

    支持流式对话问答

--- src/main/java/com/asleepyfish/controller/ChatGPTController.java
+++ src/main/java/com/asleepyfish/controller/ChatGPTController.java
@@ -1,16 +1,11 @@
 package com.asleepyfish.controller;
 
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.completion.chat.ChatMessageRole;
 import io.github.asleepyfish.util.OpenAiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -20,28 +15,41 @@ import java.util.List;
  */
 @RestController
 public class ChatGPTController {
-    @PostMapping("/chatTest")
-    public List<String> chatTest(String content) {
-        return OpenAiUtils.createChatCompletion(content, "testUser");
+    /**
+     * 普通问答
+     */
+    @GetMapping("/chat")
+    public List<String> chat(String content) {
+        return OpenAiUtils.createChatCompletion(content);
     }
 
+    /**
+     * 流式问答，返回到控制台
+     */
+    @GetMapping("/streamChat")
+    public void streamChat(String content) {
+        // OpenAiUtils.createStreamChatCompletion(content, System.out);
+        // 下面的默认和上面这句代码一样，是输出结果到控制台
+        OpenAiUtils.createStreamChatCompletion(content);
 
+    }
+
+    /**
+     * 流式问答，输出结果到WEB浏览器端
+     */
+    @GetMapping("/streamChatWithWeb")
+    public void streamChatWithWeb(String content, HttpServletResponse response) throws IOException {
+        // 需要指定response的ContentType为流式输出，且字符编码为UTF-8
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
+        OpenAiUtils.createStreamChatCompletion(content, response.getOutputStream());
+    }
+
+    /**
+     * 下载图片
+     */
     @GetMapping("/downloadImage")
     public void downloadImage(String prompt, HttpServletResponse response) {
         OpenAiUtils.downloadImage(prompt, response);
-    }
-
-    @PostMapping("/streamChatTest")
-    public void streamChatTest(String content) {
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .messages(Collections.singletonList(new ChatMessage(ChatMessageRole.USER.value(), content)))
-                .n(1)
-                .logitBias(new HashMap<>())
-                .stream(true)
-                .user("user")
-                .build();
-        OpenAiUtils.createStreamChatCompletion(chatCompletionRequest);
     }
 }
