5d43b608e1cc07bef3f7192748d2eb07fb5f12d7
asleepyfish <asleepyfish@foxmail.com>
2023-05-08 23:45:25 +0800

    update demo

--- src/main/java/com/asleepyfish/controller/ChatGPTController.java
+++ src/main/java/com/asleepyfish/controller/ChatGPTController.java
@@ -1,11 +1,14 @@
 package com.asleepyfish.controller;
 
+import io.github.asleepyfish.config.ChatGPTProperties;
+import io.github.asleepyfish.service.OpenAiProxyService;
 import io.github.asleepyfish.util.OpenAiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.List;
 
 /**
@@ -61,5 +64,20 @@ public class ChatGPTController {
         System.out.println("四月使用：" + monthUsage + "美元");
         String totalUsage = OpenAiUtils.billingUsage();
         System.out.println("一共使用：" + totalUsage + "美元");
+    }
+
+    /**
+     * 自定义Token使用（解决单个SpringBoot项目中只能指定唯一的Token[sk-xxxxxxxxxxxxx]的问题，现在可以自定义ChatGPTProperties内容，添加更多的Token实例）
+     */
+    @GetMapping("/customToken")
+    public void customToken() {
+        ChatGPTProperties chatGPTProperties = new ChatGPTProperties();
+        chatGPTProperties.setToken("sk-002xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        chatGPTProperties.setProxyHost("127.0.0.1");
+        chatGPTProperties.setProxyPort(7890);
+        OpenAiProxyService openAiProxyService = new OpenAiProxyService(chatGPTProperties, Duration.ZERO);
+        // 直接使用new出来的openAiProxyService来调用方法，每个OpenAiProxyService都拥有自己的Token。
+        // 这样在一个SpringBoot项目中，就可以有多个Token，可以有更多的免费额度供使用了
+        openAiProxyService.createStreamChatCompletion("Java的三大特性是什么");
     }
 }
