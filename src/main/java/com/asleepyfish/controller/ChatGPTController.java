28d988f566b90426a2f4385f9477464c728f3430
asleepyfish <asleepyfish@foxmail.com>
2023-05-25 17:56:06 +0800

    Revert "v1.2.1"
    This reverts commit 99eabe140f449b8cdf3a33127ada144bc03af710.

--- src/main/java/com/asleepyfish/controller/ChatGPTController.java
+++ src/main/java/com/asleepyfish/controller/ChatGPTController.java
@@ -1,8 +1,6 @@
 package com.asleepyfish.controller;
 
 import io.github.asleepyfish.config.ChatGPTProperties;
-import io.github.asleepyfish.entity.billing.Billing;
-import io.github.asleepyfish.entity.billing.Subscription;
 import io.github.asleepyfish.service.OpenAiProxyService;
 import io.github.asleepyfish.util.OpenAiUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -60,22 +58,12 @@ public class ChatGPTController {
         OpenAiUtils.downloadImage(prompt, response);
     }
 
-    @GetMapping("/billing")
-    public void billing() {
+    @GetMapping("/billingUsage")
+    public void billingUsage() {
         String monthUsage = OpenAiUtils.billingUsage("2023-04-01", "2023-05-01");
         System.out.println("四月使用：" + monthUsage + "美元");
         String totalUsage = OpenAiUtils.billingUsage();
         System.out.println("一共使用：" + totalUsage + "美元");
-        String stageUsage = OpenAiUtils.billingUsage("2023-01-31");
-        System.out.println("自从2023/01/31使用：" + stageUsage + "美元");
-        Subscription subscription = OpenAiUtils.subscription();
-        System.out.println("订阅信息（包含到期日期，账户总额度等信息）：" + subscription);
-        // dueDate为到期日，total为总额度，usage为使用量，balance为余额
-        Billing totalBilling = OpenAiUtils.billing();
-        System.out.println("历史账单信息：" + totalBilling);
-        // 默认不传参的billing方法的使用量usage从2023-01-01开始，如果用户的账单使用早于该日期，可以传入开始日期startDate
-        Billing posibleStartBilling = OpenAiUtils.billing("2022-01-01");
-        System.out.println("可能的历史账单信息：" + posibleStartBilling);
     }
 
     /**
