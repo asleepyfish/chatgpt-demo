9093e42ffd2d940dbc4fbd2e0374788278f15519
asleepyfish <asleepyfish@foxmail.com>
2023-08-06 22:26:24 +0800

    Support custom client

---
+++ src/main/java/com/asleepyfish/config/ClientConfiguration.java
@@ -0,0 +1,40 @@
+package com.asleepyfish.config;
+
+import io.github.asleepyfish.config.ChatGPTProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: asleepyfish
+ * @Date: 2023-08-06 21:52
+ * @Description: client configuration
+ */
+@Configuration
+public class ClientConfiguration {
+
+    @Resource
+    private ChatGPTProperties properties;
+
+/*    @Bean
+    public OkHttpClient okHttpClient() {
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(100);
+        dispatcher.setMaxRequestsPerHost(10);
+        return new OkHttpClient.Builder()
+                .addInterceptor(new AuthenticationInterceptor(properties.getToken()))
+                .connectionPool(new ConnectionPool(100, 10, TimeUnit.SECONDS))
+                .readTimeout(Duration.ZERO.toMillis(), TimeUnit.MILLISECONDS)
+                .connectTimeout(Duration.ZERO.toMillis(), TimeUnit.MILLISECONDS)
+                .hostnameVerifier((hostname, session) -> true)
+                .proxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress(properties.getProxyHost(), properties.getProxyPort())))
+                .proxyAuthenticator((route, response) -> {
+                    String credential = Credentials.basic("proxyUsername", "proxyPassword");
+                    return response.request().newBuilder()
+                            .header("Proxy-Authorization", credential)
+                            .build();
+                })
+                .dispatcher(dispatcher)
+                .build();
+    }*/
+}
